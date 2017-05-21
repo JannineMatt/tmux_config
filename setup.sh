f8c84896dd45ae6554857dbc48512e2b3efbef37
William <william@WorkStation>
2017-05-22 01:13:20 +0800

    add powerfont

--- setup.sh
+++ setup.sh
@@ -6,6 +6,14 @@ ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo apt install fish
+
+# setup power font
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+# setup tmux for powerfont
 echo "tmux_conf_theme_left_separator_main=''
 tmux_conf_theme_left_separator_sub=''
 tmux_conf_theme_right_separator_main=''
